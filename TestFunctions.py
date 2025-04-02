6df82a6487476616fe272f8c0be85e6efdc4c09e
NikolaiHaim <63632906+NikolaiHaim@users.noreply.github.com>
2025-04-02 13:16:30 +0200

    Update TestFunctions.py
    More test matrices 02-04-2025

--- TestFunctions.py
+++ TestFunctions.py
@@ -1,4 +1,5 @@
 import MatrixMath
+import numpy
 
 matrix0 = [[1]]
 
@@ -42,6 +43,12 @@ matrix9 = [[1,0,0,0],
            [0,0,11,0],
            [0,0,0,16]]
 
+matrix10 = [[1],
+            [2],
+            [3]]
+
+matrix11 = [[1,2,3]]
+
 listofmatrices = []
 
 listofmatrices.append(matrix0)
@@ -57,17 +64,17 @@ listofmatrices.append(matrix9)
 
 
 for x in listofmatrices:
-    #print("Matrix " + str(listofmatrices.index(x)+1) + ": " + str(MatrixMath.IsSquare(x)))
-    #print("Matrix " + str(listofmatrices.index(x)+1) + ": " + str(MatrixMath.IsUpperTriangular(x)))
-    #print("Matrix " + str(listofmatrices.index(x)+1) + ": " + str(MatrixMath.IsLowerTriangular(x)))
-    #print("Matrix " + str(listofmatrices.index(x)+1) + ": " + str(MatrixMath.IsDiagonal(x))) 
-    #print("Matrix " + str(listofmatrices.index(x)+1) + ": " + str(AssistanceFunctions.shape(x)))
-    if MatrixMath.IsSquare(x):
-        print("Matrix " + str(listofmatrices.index(x)) + " Determinant: " + str(MatrixMath.DeterminantOfMatrix(x)))
-    print("")
+    break
+    #print("Matrix " + str(listofmatrices.index(x)) + ": " + str(MatrixMath.IsSquare(x)))
+    #print("Matrix " + str(listofmatrices.index(x)) + ": " + str(MatrixMath.IsUpperTriangular(x)))
+    #print("Matrix " + str(listofmatrices.index(x)) + ": " + str(MatrixMath.IsLowerTriangular(x)))
+    #print("Matrix " + str(listofmatrices.index(x)) + ": " + str(MatrixMath.IsDiagonal(x))) 
+    #print("Matrix " + str(listofmatrices.index(x)) + ": " + str(AssistanceFunctions.shape(x)))
+    #print("Matrix " + str(listofmatrices.index(x)) + " Determinant: " + str(MatrixMath.DeterminantOfMatrix(x)))
+    #print("")
     #print(AssistanceFunctions.shape(x))
 
-#MatrixMath.DeterminantOfMatrix(matrix6)
+#MatrixMath.DeterminantOfMatrix(matrix5)
 
 #print(MatrixMath.IsSquare(matrix9))
 #print(MatrixMath.IsLowerTriangular(matrix9))
@@ -75,3 +82,27 @@ for x in listofmatrices:
 #print(MatrixMath.IsDiagonal(matrix9))
 # print(matrix1[0].index(2))
 
+A = [[2, 1, 3],
+     [1, 2, 1],
+     [3, 1, 2]]
+
+B = [[4,7,2],
+     [3,6,1],
+     [2,5,3]]
+
+#print(MatrixMath.shape(matrix6))
+#print(MatrixMath.shape(matrix5))
+print(MatrixMath.MatrixMultiplication2(matrix11, matrix5))
+
+#print(MatrixMath.InverseMatrix(B))
+#print(numpy.linalg.inv(B))
+
+#print(matrix6)
+#print(MatrixMath.ScalarMultiplication(matrix6, 2))
+
+#print(matrix6)
+#print(MatrixMath.TransposeMatrix(matrix6))
+
+#print(MatrixMath.columnStripper(matrix5, 1))
+
+#print(MatrixMath.TransposeMatrix([24.0, 28.9, 28.9, 29.0, 29.1, 29.1, 29.2, 29.2, 29.3, 29.4]))
